357368e1b5d878aaaa55815286a17aabb3decdc9
Nitin Sahu <ns0779687@gmail.com>
2024-12-10 22:19:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('.navbar a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
-    });
-});
+
